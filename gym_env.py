da0a2b8de56985dd1f7481755bea18fc20703d87
bulletcross <bulletcross@gmail.com>
2018-03-21 22:16:14 +0530

    added environment class file

---
+++ gym_env.py
@@ -0,0 +1,44 @@
+'''
+Environment class definition.
+Reference: https://gym.openai.com/docs/
+Author: bulletcross@gmail.com (Vishal Keshav)
+'''
+
+import gym
+import numpy as np
+
+class gym_env(object):
+    def __init__(self, env_name):
+		'''
+		Initialize environment, action space and observation space
+		'''
+        self.env = gym.make(env_name)
+		self.env.seed(3)
+        self.action_space = self.env.action_space
+        self.observation_space = self.env.observation_space
+		#Output the observation to monitor
+        self.env = gym.wrappers.Monitor(self.env, ./, force=True)
+
+    def reset(self):
+        '''
+		Reset the environment and returns array the observations
+        '''
+        observation = self.env.reset()
+        return observation
+
+    def next(self,action):
+        '''
+		Given an action, step up the environment
+        '''
+        if not self.env.action_space.contains(action):
+            print("Invalid action")
+        self.env.render()
+
+        observation, reward, done, info = self.env.step(action)
+        return observation, reward, done, info
+
+    def print_action_space(self):
+        print(self.action_space)
+
+    def print_observation_space(self):
+        print(self.observation_space)
