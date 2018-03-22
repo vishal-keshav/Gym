02558ebaa9bf107d11d363148262ec4322b6f5d9
bulletcross <bulletcross@gmail.com>
2018-03-22 22:32:08 +0530

    atari games

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ Crated by: bulletcross@gmail.com (Vishal Keshav)
 import gym_env
 
 def main():
-    env = gym_env.gym_env('CartPole-v0')
-    env.reset()
-    for _ in range(100000):
-        action = env.get_action_space()
-        observation, reward, done, _ = env.next(action.sample())
-        if done:
-            break
-        print(observation)
+    env = gym_env.gym_env('SpaceInvaders-v0')
+    for e in range(10):
+        observation = env.reset()
+        for t in range(1000):
+            action = env.get_action_space()
+            observation, reward, done, _ = env.next(action.sample())
+            if done:
+                print("*************")
+                break
+            print(observation)
 
 if __name__ == "__main__":
     main()
