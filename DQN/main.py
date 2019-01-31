11618d3d34e1c1c926392f3f20131d9f623c93a5
Vishal Keshav <vishal.keshav.1993@gmail.com>
2019-01-31 23:32:54 +0530

    update

--- DQN/main.py
+++ DQN/main.py
@@ -1,14 +1,38 @@
-"""
-Crated by: bulletcross@gmail.com (Vishal Keshav)
-"""
-import gym_env
-import net_param
-import deep_q_network
 import numpy as np
+import argparse
+
+import gym_env
+import deep_q_network
+
 #210,160,3
 #0,1,2,3,4,5
 
+def argument_parser():
+    parser = argparse.ArgumentParser(description = "Testing DQN networks")
+    parser.add_argument('--env_name', default = 'SpaceInvaders-v0', type = str,
+                    help = 'agent from gym')
+    parser.add_argument('--buffer_size', default = 8, type = int,
+                    help = 'size of circuler buffer storing (s,a,r,s) tuple')
+    parser.add_argument('--exploration_probability', default = 0.1,
+                    type = float, help = 'probability to randomly take action')
+    parser.add_argument('--max_train_steps', default = -1, type = int,
+                    help = 'maximum number of observation, -1 for infinity')
+    parser.add_argument('--nr_episodes', default = 100, type = int,
+                    help = 'number of episodes to train the agent')
+    parser.add_argument('--monitor_training', default = True, type = bool,
+                    help = 'display the training environment')
+    parser.add_argument('--lr_rate', default = 0.001, type = 'float',
+                    help = 'learning rate for training the local DQN network')
+    parser.add_argument('--discount_factor', default = 0.5, type = float,
+                    help = 'discount factor for reward calculation')
+    args = parser.parse_args()
+    return args
+
 def main():
+    args = argument_parser()
+
+
+
     env = gym_env.gym_env('SpaceInvaders-v0')
     param = {'learning_rate': 0.001, 'gamma': 0.5}
     learning_step = 32
